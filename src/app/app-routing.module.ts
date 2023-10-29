a0663b3cabc3d2650dd9e240de2bc7969cb15dbd
Lambrette Florent <florent.lambrette@gmail.com>
2023-10-29 19:11:16 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+//angular lit de haut en bas donc attention a l'ordre !
+const routes: Routes = [
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon/:id', component: DetailPokemonComponent},
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pokemon-list';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root', //id du composant
   templateUrl: 'app.component.html'
   //template: `<h1>Pokemon list !</h1>` //direct ou templateurl pour fichier html
 })
-export class AppComponent implements OnInit {
-
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemon :Pokemon) {
-    /* previous way to do it with id as parameter
-    const index :number = Number(pokemonId);
-    const pokemon : Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == index);
-
-    if (pokemon) {
-      console.log(`you have clicked on ${pokemon.name}`);
-    } else {
-      console.log(`Pokemon doesn't exist with id ${index}`);
-    }*/
-    console.log(`you have clicked on ${pokemon.name}`);
-    this.pokemonSelected = pokemon;
-  }
-}
+export class AppComponent {}
